f2d7f3883bc22de8261ed217b358742c96d556c4
Shyam Anand <shyamwdr@gmail.com>
2023-03-06 18:39:52 +0530

    added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10
+WORKDIR /usr/src/openai-speech-to-text
+COPY .env .
+COPY *.py .
+COPY static/* ./static/
+COPY templates/* ./templates/
+COPY requirements.txt .
+RUN python -m venv venv
+RUN . venv/bin/activate
+RUN pip install -r requirements.txt
+RUN pip install -U openai
+CMD ["flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -2,32 +2,39 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+import traceback
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def save_file(audiofile):
-    if audiofile.filename != '':
-        audiofile.save(audiofile.filename)
+def save_file(uploaded_file):
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+        return uploaded_file.filename
     else:
-        raise ValueError('Invalid file ' + audiofile.filename)
+        raise ValueError('Invalid file ' + uploaded_file.filename)
 
 def transcribe(file_path: str):
-    speech = open(file_path, "rb")
-    return openai.Audio.transcribe("whisper-1", speech)
+    audio = open(file_path, "rb")
+    print(audio)
+    return openai.Audio.transcribe("whisper-1", audio)
     
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        audiofile = request.files["audiofile"]
-        save_file(audiofile)
-        transcript = transcribe(audiofile.filename)
-        print(f'transcript: "{transcript.text}"')
-        return render_template("index.html", transcript=transcript.text)
+        audiofile = save_file(request.files["audiofile"])
+        try:
+            transcript = transcribe(audiofile)
+            print(f'transcript: "{transcript.text}"')
+            return redirect(url_for("index", transcript=transcript.text))
+        except Exception as e:
+            traceback.print_exc()
+            return redirect(url_for("index", error=e))
     
-    # result = request.args.get("result")
-    return render_template("index.html")
+    transcript = request.args.get("transcript")
+    error = request.args.get("error")
+    return render_template("index.html", transcript=transcript, error=error)
 
 
 def generate_prompt(animal):
